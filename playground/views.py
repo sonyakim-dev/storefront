fb300ec1e5f698d808483abead3d98f53d73ed33
sonyakim-dev <sonyakim.dev@gmail.com>
2022-11-05 00:08:17 -0700

    init django project

---
+++ playground/views.py
@@ -0,0 +1,7 @@
+# request handler; request -> response
+
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def say_hello(request):
+  return render(request, 'hello.html', {'name': 'Sonya'})
